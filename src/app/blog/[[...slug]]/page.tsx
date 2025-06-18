b4256e4dc5307ea806b9c70db2e87d2917c7ec57
Manikandan R <Manikandan.R+amway@amway.com>
2025-06-18 10:16:19 +0530

    static param fro s3

--- src/app/blog/[[...slug]]/page.tsx
+++ src/app/blog/[[...slug]]/page.tsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+  return [
+    { slug: [] }, // matches /blog
+    { slug: ['nested', 'example'] },
+    { slug: ['deep', 'path', 'post'] }
+  ];
+}
+
 export default function CatchAll({ params }: { params: { slug?: string[] } }) {
   return <pre>{JSON.stringify(params, null, 2)}</pre>;
 }
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,10 @@
+
+export async function generateStaticParams() {
+  return [
+    { slug: 'post-1' },
+    { slug: 'post-2' }
+  ];
+}
 export default function BlogPost({ params }: { params: { slug: string } }) {
   return <h2>Blog Post: {params.slug}</h2>;
 }
