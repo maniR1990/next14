c65341bb1e2ee19eaf517c35876d11684f1c8620
Manikandan <mani.r16@gmail.com>
2025-06-28 21:15:48 +0530

    google-analytics fix 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-// 📁 CLIENT: src/app/page.tsx
+"use client";
 
 import { event } from "@/lib/gtag";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>AWS pipe line success</h1>
-      <span>testing in progress</span>
+      <h1>AWS pipeline success</h1>
+      <span>Testing in progress</span>
       <button
         onClick={() =>
           event("signup_click", {
