f9393f7174ed157991336339643bdedc6ccf972f
Manikandan <mani.r16@gmail.com>
2025-06-28 17:05:01 +0530

    google-analytics

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,12 +1,18 @@
-// src/lib/gtag.ts
-export const GA_MEASUREMENT_ID = 'G-QTG7KF18GL'; // Replace with your GA ID
+export const GA_MEASUREMENT_ID = "G-QTG7KF18GL"; // Replace with your GA ID
 
 export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
+  window.gtag("config", GA_MEASUREMENT_ID, {
     page_path: url,
   });
 };
 
-export const event = (action: string, params: Record<string, any>) => {
-  window.gtag('event', action, params);
+interface GAEventParams {
+  event_category?: string;
+  event_label?: string;
+  value?: number;
+  [key: string]: string | number | undefined;
+}
+
+export const event = (action: string, params: GAEventParams) => {
+  window.gtag("event", action, params);
 };
